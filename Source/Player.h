aad3c9daef76ec2e475527a26bdde335b02551c8
佐藤友哉 <240060@jc-21.jp>
2025-06-20 10:02:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Source/Player.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Object2D.h"
+
+class Player : public Object2D {
+public:
+	Player();
+	Player(VECTOR2 pos);
+	~Player();
+	void Update() override;
+	void Draw() override;
+private:
+	float velocityY;
+	bool onGround;
+	bool prevPushed;
+
+	float Gravity;
+	float JumpHeight;
+	float JumpV0;
+	float moveSpeed;
+};
